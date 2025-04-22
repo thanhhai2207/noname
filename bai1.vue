c4dd490977cb8352eca845130cbc125fa3f3c641
thanhhai2207 <75520282+thanhhai2207@users.noreply.github.com>
2025-04-22 20:03:39 +0700

    Add files via upload

---
+++ bai1.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Chào mừng đến với VueJS</h1>
+    </template>
+    <style>
+    h1 { color: red; }
+    </style>
+
+
+
+    <script setup>
+    import Bai2 from "./Labs/Lab-1/Bai2.vue";
+    </script>
+    <template>
+    <Bai2 />
+    </template>
