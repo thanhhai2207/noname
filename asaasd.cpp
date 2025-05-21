3f61b363064f1dfa997cd86c16053cccd385731e
thanhhai2207 <75520282+thanhhai2207@users.noreply.github.com>
2025-05-21 22:25:15 +0700

    Add files via upload

---
+++ asaasd.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//Tinh va in ra tong 2 so nhap tu ban phim
+// B1: Khai bao ham
+void tinhVaInTong2So () {
+    int a,b, tong;
+    //Nhap 2 so
+    printf("a = ");
+    scanf("%d", &a);
+    printf("b=");
+    scanf("%d," &b);
+    // tinh tong
+    tong = a +b;
+    // in ra man hinh 
+    printf("Tong = %d, tong");
+
+
+} 
+
+int main () {
+
+
+
+
+
+    return 0;
+}
